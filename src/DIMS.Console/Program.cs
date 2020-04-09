fccba94eb8ee775e8ee23df9840596f0071e1fa1
Gravypower <aaron@gravypower.net>
2020-04-09 20:22:18 +1000

    Play with ideas

---
+++ src/DIMS.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using DIMS.Engine;
+
+namespace DIMS.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Class1();
+            t.test();
+        }
+    }
+}
---
+++ src/DIMS.Engine/ITopology.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIMS.Engine
+{
+    public interface ITopology
+    {
+        IList<string> Images { get; }
+    }
+}
