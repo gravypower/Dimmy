186aab66d8b22729ba0fcd24916ff373e298f762
Gravypower <aaron@gravypower.net>
2020-04-25 14:29:00 +1000

    Fixed up so project init command available when added via a plugin

--- src/Dimmy.Sitecore.Plugin/Plugin.cs
+++ src/Dimmy.Sitecore.Plugin/Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using ClassLibrary1;
 using Dimmy.Cli;
+using Dimmy.Cli.Commands.Project;
 using SimpleInjector;
 
 namespace Dimmy.Sitecore.Plugin
@@ -9,6 +10,8 @@ namespace Dimmy.Sitecore.Plugin
         public void Bootstrap(Container container)
         {
             container.Collection.Register<ITopology>(GetType().Assembly);
+
+            container.Collection.Append<InitialiseSubCommand, InitialiseSitecore>();
 
             new Class1().DoSomething();
         }
