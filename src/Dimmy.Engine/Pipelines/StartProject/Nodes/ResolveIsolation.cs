12c9b9437399eba625031c3ec7f71bfea7959c2a
Gravypower <aaron@gravypower.net>
2020-08-19 20:20:01 +1000

    Added dimmy Ascii Art
    Started to look at working on isolation resolution

---
+++ src/Dimmy.Engine/Pipelines/StartProject/Nodes/ResolveIsolation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace Dimmy.Engine.Pipelines.StartProject.Nodes
+{
+    public class ResolveIsolation:Node<IStartProjectContext>
+    {
+        public override void DoExecute(IStartProjectContext input)
+        {
+            if(!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return;
+
+
+            using var key = Registry.CurrentUser.OpenSubKey(@"Software\\Microsoft\\Windows NT\\CurrentVersion");
+            if (key == null) return;
+            
+            var releaseId = (string)key.GetValue("ReleaseId");
+            
+            
+            
+        }
+    }
+}
